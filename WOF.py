3100652d1a2f2ad4dc9c29e2daf06cf4a0bdc13c
ChrisMCodes <34985965+ChrisMCodes@users.noreply.github.com>
2019-11-11 21:04:38 -0700

    Update WOF.py

--- WOF.py
+++ WOF.py
@@ -18,29 +18,49 @@ class WOFPlayer:
         return '{} (${})'.format(self.name, self.prizeMoney)
 # Write the WOFHumanPlayer class definition (part B) here
 class WOFHumanPlayer(WOFPlayer):
-    def __init__(self, category, obscuredPhrase, guessed):
-        self.getMove = input('{} has ${}\n\n'.format(self.name, self.prizeMoney), 'Category: {}\n'.format(category), 'Phrase: {}\n'.format(obscuredPhrase), 'Guessed: {}\n\n'.format(guessed), 'Guess a letter, phrase, or type \'exit\' or \'pass\':') 
+    def getMove(self, category, obscuredPhrase, guessed):
+        print("{} has ${}\n".format(self.name, self.prizeMoney))
+        print("Category: {}".format(category))
+        print("Phrase: {}".format(phrase))
+        print("Guessed: {}\n".format(guessed))
+        moveInput = input("Guess a letter, phrase, or type 'exit' or 'pass':")
+        return moveInput
 # Write the WOFComputerPlayer class definition (part C) here
 class WOFComputerPlayer(WOFPlayer):
+    SORTED_FREQUENCIES = 'ZQXJKVBPYGFWMUCLDRHSNIOATE'
     def __init__(self, name, difficulty):
-        self.SORTED_FREQUENCIES = 'ZQXJKVBPYGFWMUCLDRHSNIOATE'
+        self.name = name
         self.difficulty = difficulty
+        WOFPlayer.__init__(self, name)
+   
     def smartCoinFlip(self):
-        r_num = random.randint(1, 10)
-        if r_num > self.guilty:
+        ran = random.randint(1, 10)
+        if ran > self.difficulty:
             return True
         else:
             return False
+
     def getPossibleLetters(self, guessed):
-        poss = []
-        for letter in LETTERS:
-            if letter not in guessed:
-                if self.vowel_cost >= 250:
-                    poss = poss + [letter]
-                else:
-                    if letter not in VOWELS:
-                        poss = poss + [letter]
-        return poss
+        guessed_list = [ch for ch in guessed]
+        if self.prizeMoney <= VOWEL_COST:
+            letter_list = []
+            for cha in LETTERS:
+                if cha not in guessed_list:
+                    if cha not in VOWELS:
+                        letter_list.append(cha)
+        else:
+            letter_list = [cha for cha in LETTERS if cha not in guessed_list]
+        return letter_list
+    
     def getMove(self, category, obscuredPhrase, guessed):
-        if len(self.getPossibleLetters) == 0:
+        play = WOFComputerPlayer.SORTED_FREQUENCIES[::-1]
+        letters_possible = self.getPossibleLetters(guessed)
+        if len(letters_possible) == 0:
             return 'pass'
+        else:
+            coinFlipResp = self.smartCoinFlip()
+            if coinFlipResp == True:
+                if play not in guessed:
+                    return play
+            if coinFlipResp == False:
+                return random.choice(letters_possible)
